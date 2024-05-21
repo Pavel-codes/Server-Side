98f7065373e78346c80736352c87d07f86a517b5
ChenAhrak <chenahrak@gmail.com>
2024-05-21 19:34:39 +0300

    Added a Class and Controller Users. Not finish.

---
+++ ex2-server/WebApplication1/BL/User.cs
@@ -0,0 +1,81 @@
+﻿namespace WebApplication1.BL
+{
+    public class User
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string password;
+        private bool isAdmin = false;
+        private bool isActive = true;
+        
+        static List<Course> myCourses = new List<Course>();
+        static List<User> usersList = new List<User>();
+        
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public bool IsAdmin { get => isAdmin; set => isAdmin = value; } 
+        public bool IsActive { get => isActive; set => isActive = value; }
+        public static List<Course> MyCourses { get => myCourses; set => myCourses = value; }
+        public static List<User> UsersList { get => usersList; set => usersList = value; }
+
+        public User() {
+            if (usersList.Count == 0)
+                usersList.Add(new User(Id = 1, Name = "admin", Email = "admin@admin.com", Password = "admin", IsAdmin = true, IsActive = true));
+        }
+
+        public User(int id, string name, string email, string password)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.isAdmin = false; 
+            this.isActive = true; 
+        }
+
+        public User(int id, string name, string email, string password, bool isAdmin, bool isActive)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.isAdmin = isAdmin;
+            this.isActive = isActive;
+        }
+
+
+        public List<User> getUsers()
+        {
+            return usersList;
+        }
+        public bool registration()
+        {
+                foreach (User user in usersList)
+                {
+                    if (user.Id == this.Id || user.Password.Equals(this.Password)) 
+                            return false;
+
+                }
+               
+                usersList.Add(this);
+                return true;
+                   
+        }
+
+        public bool login()
+        {
+            foreach (User user in usersList)
+            {
+                if (user.Id == this.Id && user.Password.Equals(this.Password))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }   
+
+    }
+}
---
+++ ex2-server/WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.BL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        User user = new User();
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+             return user.getUsers();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public bool Post([FromBody] User value)
+        {
+            return value.registration();
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
